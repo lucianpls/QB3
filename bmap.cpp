7ef0b37f04fe8c23874b850a884eec05ce0534f8
Lucian Plesea <lucianpls@gmail.com>
2020-03-21 12:50:11 -0700

    Add RLE, better quart encoding

--- bmap.cpp
+++ bmap.cpp
@@ -18,18 +18,53 @@ BMap::BMap(int x, int y) : _x(x), _y(y), _lw((x + 7) / 8) {
 	_v.assign(_lw * ((y + 7) / 8), ~0); // All data
 }
 
-int rlen(uint8_t* ch, size_t mlen) {
+int rlen(const uint8_t* ch, size_t mlen) {
 	int r = 0;
-	while (mlen-- && ch[0] == ch[r]) { r++; }
+	while (mlen-- && ch[0] == ch[r])
+		r++;
 	return r;
 }
 
-size_t BMap::pack(BitstreamOut &s) {
+void RLE(std::vector<uint8_t> &v, std::vector<uint8_t>& result) {
+	// Then do an RLE across everything
+	// This is buggy, needs a lot more tweaks
+
+	std::vector<uint8_t> compr;
+
+#define CODE 0xC5
+	size_t len = 0;
+	while (len < v.size()) {
+		int l = rlen(&v[len], v.size() - len);
+		uint8_t c = v[len];
+		if (l < 4) {
+			l = 1;
+			if (CODE == c) {
+				compr.push_back(CODE);
+				compr.push_back(0);
+			}
+			else {
+				compr.push_back(c);
+			}
+		}
+		else {
+			compr.push_back(CODE);
+			if (l > 255)
+				compr.push_back(l >> 8);
+			compr.push_back(l);
+			compr.push_back(c);
+		}
+		len += l;
+	}
+	result.swap(compr);
+}
+
+size_t BMap::pack(BitstreamOut& s) {
 	for (auto it : _v) {
 		if (0 == it || ~0 == it) {
 			s.push(it & 0b11, 2);
 		}
 		else {
+			// If a single 16 bit quart is 0 or ~0, it's worth subdividing
 			bool quarts = false;
 			uint16_t q[4];
 			for (int i = 0; i < 4; i++) {
@@ -38,14 +73,19 @@ size_t BMap::pack(BitstreamOut &s) {
 					quarts = true;
 			}
 			if (quarts) {
+				// subdivide the 64bit int in quarters
 				s.push(0b10, 2);
 				for (int i = 0; i < 4; i++)
+					// Codes 00 and 11 means 0000 and ffff
 					if (0 == q[i] || 0xffff == q[i]) {
 						s.push(q[i] & 0b11, 2);
 					}
 					else {
-						s.push(0b01, 2);
-						s.push(q[i], 16);
+						// We have two codes left, but subdiving doesn't make sense.
+						// So we just use the codes to encode the top bit
+						// 01 means bit 15 is 1, 10 means bit 15 is 0
+						s.push((q[i] > 0x7fff) ? 0b01 : 0b10, 2);
+						s.push(q[i] & 0x7fff, 15);
 					}
 			}
 			else {
@@ -55,35 +95,7 @@ size_t BMap::pack(BitstreamOut &s) {
 		}
 	}
 
-	// Then do an RLE across everything
-	// This is buggy, needs a lot more tweaks
-
-	std::vector<uint8_t> compr;
-
-#define CODE 0xC5
-	size_t len = 0;
-	while (len < s.v.size()) {
-		int l = rlen(&s.v[len], s.v.size() - len);
-		if (l < 4) {
-			l = 1;
-			if (CODE == s.v[len]) {
-				compr.push_back(CODE);
-				compr.push_back(0);
-			}
-			else {
-				compr.push_back(s.v[len]);
-			}
-		}
-		else {
-			// Missing the long sequence treatment, which is not common
-//			std::cout << l << std::endl;
-			compr.push_back(CODE);
-			if (l > 255)
-				compr.push_back(l >> 8);
-			compr.push_back(l);
-			compr.push_back(s.v[len]);
-		}
-		len += l;
-	}
-	return compr.size();
+	return s.v.size();
 }
+
+const uint8_t BitstreamIn::usemask[8] = {0, 0b1, 0b11, 0b111, 0b1111, 0b11111, 0b111111, 0b1111111};
--- bmap.h
+++ bmap.h
@@ -3,9 +3,11 @@
 #include <cinttypes>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 #if defined(_DEBUG)
 #include <iostream>
+#include <cstdio>
 
 struct BStream {
 	BStream() : len(0) {};
@@ -41,6 +43,28 @@ struct BitstreamOut {
 	}
 };
 
+struct BitstreamIn {
+	std::vector<uint8_t> v;
+	size_t pos; // Next bit position
+	BitstreamIn(std::vector<uint8_t>& other) :
+		v(other), pos(0) {};
+	template<typename T = uint64_t> bool pull(T& val, size_t bits) {
+		uint64_t acc = 0;
+		while (bits && ((pos / 8) < v.size())) {
+			size_t use = std::min(8 - pos % 8, bits);
+			acc = (acc << use) | ((v[pos / 8] >> (pos % 8)) & usemask[use]);
+			bits -= use;
+			pos += use;
+		}
+		if (0 != bits)
+			return false;
+		val = static_cast<T>(acc);
+		return true;
+	}
+private:
+	static const uint8_t usemask[8];
+};
+
 class BMap {
 public:
 	BMap(int x, int y);
@@ -56,13 +80,22 @@ public:
 	}
 	size_t pack(BitstreamOut & stream);
 	size_t dsize() { return _v.size()*sizeof(uint64_t); }
-	void getsize(int& x, int& y) {
-		x = _x; y = _y;
+	void getsize(int& x, int& y) { x = _x; y = _y; }
+	void dump(const std::string& name) {
+		FILE* f = fopen(name.c_str(), "wb");
+		fwrite(_v.data(), _v.size(), sizeof(uint64_t), f);
+		fclose(f);
 	}
 private:
-	size_t unit(int x, int y) { return _lw * (y / 8) + x / 8; }
-	int bitl(int x, int y) { return _xy[((y & 7) << 3) + (x & 7)]; }
+	size_t unit(int x, int y) {
+		return _lw * (y / 8) + x / 8;
+	}
+	int bitl(int x, int y) {
+		return _xy[((y & 7) << 3) + (x & 7)];
+	}
 	int _x, _y;
 	size_t _lw; // Line width
 	std::vector<uint64_t> _v;
 };
+
+void RLE(std::vector<uint8_t>& v, std::vector<uint8_t> &result);
