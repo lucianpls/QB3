06a43205aa2af6b6b754ad989237a0aee3ee1f24
Lucian Plesea <lucianpls@gmail.com>
2020-03-31 09:51:57 -0700

    Moved the encoding out of main
    Got rid of the delta vector,  replaced with local static function

--- denc.cpp
+++ denc.cpp
@@ -1,34 +1,211 @@
 #include "denc.h"
+#include "bmap.h"
+#include <iostream>
 
-int Denc::delta(const std::vector<uint8_t> &input) {
-	auto pval = input.data();
-	for (size_t i = 0; i < input.size() / bands; i++) {
-		for (size_t c = 0; c < bands; c++) {
-			v.push_back(*pval - prev[c]);
-			prev[c] = *pval++;
-		}
-	}
-	return 0;
+using namespace std;
+
+// Delta and by low sign encoding
+static int dsign(std::vector<uint8_t>& v, uint8_t prev) {
+    for (auto& it : v) {
+        swap(it, prev);
+        it = prev - it;
+        it = (it & 0x80) ? -(2 * it + 1) : (2 * it);
+    }
+    return prev;
 }
 
-int Denc::deltacheck(const std::vector<uint8_t>& input) {
-	for (auto& it : prev) it = 127; // Restore defaults
-	auto expected = input.data();
-	auto d = v.data();
-	for (size_t i = 0; i < input.size() / bands; i++) {
-		for (size_t c = 0; c < bands; c++) {
-			uint8_t val = *d++ + prev[c];
-			if (val != *expected)
-				return 1;
-			prev[c] = *expected++;
-		}
-	}
-	return 0;
-}
+size_t encode(std::vector<uint8_t> &image, 
+    size_t xsize, size_t ysize, size_t bsize)
+{
+    //Denc deltaenc;
+//deltaenc.delta(image);
+//deltaenc.deltacheck(image);
+//deltaenc.recode();
+// This isn't right, just to get an idea
+//vector<uint8_t>& source = deltaenc.v;
 
-int Denc::recode() {
-	for (auto& it : v)
-//		it = (it & 0x80) ? (0xff - 2 * it) : (2 * it);
-		it = (it & 0x80) ? -(2 * it + 1) : (2 * it);
-return 0;
+    Bitstream s;
+    vector<size_t> hist(256);
+    vector<size_t> bithist(8);
+    // The sizes are multiples of 8, no need to check
+    size_t bands = image.size() / xsize / ysize;
+    vector<uint8_t> prev(bands, 127); // RGB
+    // This should be 8, for noisy images 4 is better
+    // 2 generates too much overhead
+    // 16 might work for slow varying inputs, 
+    // which would need the lookup tables extended
+    // Works for non-power of two blocks, using custom tables
+
+    static const uint8_t xp2[64] = {
+        0, 1, 0, 1, 2, 3, 2, 3,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        4, 5, 4, 5, 6, 7, 6, 7,
+        4, 5, 4, 5, 6, 7, 6, 7,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        4, 5, 4, 5, 6, 7, 6, 7,
+        4, 5, 4, 5, 6, 7, 6, 7
+    };
+    static const uint8_t yp2[64] = {
+        0, 0, 1, 1, 0, 0, 1, 1,
+        2, 2, 3, 3, 2, 2, 3, 3,
+        0, 0, 1, 1, 0, 0, 1, 1,
+        2, 2, 3, 3, 2, 2, 3, 3,
+        4, 4, 5, 5, 4, 4, 5, 5,
+        6, 6, 7, 7, 6, 6, 7, 7,
+        4, 4, 5, 5, 4, 4, 5, 5,
+        6, 6, 7, 7, 6, 6, 7, 7
+    };
+
+    static const uint8_t x3[9] = {
+        0, 1, 0, 1, 2, 2, 0, 1, 2
+    };
+    static const uint8_t y3[9] = {
+        0, 0, 1, 1, 0, 1, 2, 2, 2
+    };
+
+    static const uint8_t x5[25] = {
+        0, 1, 0, 1, 2, 3, 2, 3,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        4, 4, 4, 4,
+        0, 1, 2, 3, 4
+    };
+
+    static const uint8_t y5[25] = {
+        0, 0, 1, 1, 0, 0, 1, 1,
+        2, 2, 3, 3, 2, 2, 3, 3,
+        0, 1, 2, 3,
+        4, 4, 4, 4, 4
+    };
+
+    static const uint8_t x6[36] = {
+        0, 1, 0, 1, 2, 3, 2, 3,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        4, 5, 4, 5, 4, 5, 4, 5,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        4, 5, 4, 5
+    };
+
+    static const uint8_t y6[36] = {
+        0, 0, 1, 1, 0, 0, 1, 1,
+        2, 2, 3, 3, 2, 2, 3, 3,
+        0, 0, 1, 1, 2, 2, 3, 3,
+        4, 4, 5, 5, 4, 4, 5, 5,
+        4, 4, 5, 5
+    };
+
+    static const uint8_t x7[49] = {
+        0, 1, 0, 1, 2, 3, 2, 3,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        4, 5, 4, 5, 6, 6,
+        4, 5, 4, 5, 6, 6,
+        0, 1, 0, 1, 2, 3, 2, 3,
+        0, 1, 2, 3,
+        4, 5, 4, 5, 6, 6,
+        4, 5, 6,
+    };
+
+    static const uint8_t y7[49] = {
+        0, 0, 1, 1, 0, 0, 1, 1,
+        2, 2, 3, 3, 2, 2, 3, 3,
+
+        0, 0, 1, 1, 0, 1,
+        2, 2, 3, 3, 2, 3,
+        4, 4, 5, 5, 4, 4, 5, 5,
+        6, 6, 6, 6,
+        4, 4, 5, 5, 4, 5,
+        6, 6, 6
+    };
+
+    const uint8_t* xlut = xp2;
+    const uint8_t* ylut = yp2;
+    switch (bsize) {
+    case(3):
+        xlut = x3;
+        ylut = y3;
+        break;
+    case(5):
+        xlut = x5;
+        ylut = y5;
+        break;
+    case(6):
+        xlut = x6;
+        ylut = y6;
+        break;
+    case(7):
+        xlut = x7;
+        ylut = y7;
+        break;
+    }
+
+    vector<uint8_t> group(bsize * bsize);
+    for (int y = 0; (y + bsize) <= ysize; y += bsize) {
+        for (int x = 0; (x + bsize) <= xsize; x += bsize) {
+            size_t loc = (y * xsize + x) * 3;
+            for (int c = 0; c < 3; c++) {
+
+                for (int i = 0; i < group.size(); i++)
+                    group[i] = image[loc + c + (ylut[i] * xsize + xlut[i]) * 3];
+
+                // Round up to nearest odd (negative) value
+                // Saves writing that last bit on every block
+                // Expends one extra bit on two specific values
+                // if they are present, by moving the cutof value 
+                // down by one
+                // The net effect decreases output size
+
+                // Delta with low sign encode
+                prev[c] = dsign(group, prev[c]);
+                uint64_t maxval = 1 | *max_element(group.begin(), group.end());
+
+                // Encode the maxval
+                // Number of bits after the fist 1
+                size_t bits = ilogb(maxval);
+                bithist[bits]++;
+                hist[maxval]++;
+
+                // Push the middle bits of maxval, prefixed by the number of bits
+                if (0 == bits) { // All values are 0 or 1
+                    s.push(0, 3);
+                    uint64_t val = 0;
+                    for (auto it = group.rbegin(); it != group.rend(); it++)
+                        val = val * 2 + *it;
+                    s.push(val, group.size());
+                    continue;
+                }
+
+                s.push((maxval & (Bitstream::mask[bits] - 1)) * 4 + bits, bits + 2);
+
+                auto cutof = Bitstream::mask[bits + 1] - maxval;
+                if (0 == cutof) { // Uniform length codewords
+                    bits++;
+                    for (auto val : group)
+                        s.push(val, bits);
+                    continue;
+                }
+
+                for (uint64_t val : group) {
+                    if (val < cutof) { // Truncated
+                        s.push(val, bits);
+                        continue;
+                    }
+                    // Adjust, rotate and store the value
+                    val += cutof;
+                    val = (val >> 1) + ((val & 1) << bits);
+                    s.push(val, bits + 1);
+                }
+            }
+        }
+    }
+
+    FILE* f;
+    fopen_s(&f, "out.raw", "wb");
+    fwrite(s.v.data(), 1, s.v.size(), f);
+    for (int i = 0; i < hist.size(); i++)
+        cout << i << "," << hist[i] << endl;
+
+    for (int i = 0; i < bithist.size(); i++)
+        cout << i << "," << bithist[i] << endl;
+    fclose(f);
+    return s.v.size();
 }
--- denc.h
+++ denc.h
@@ -2,19 +2,8 @@
 #include <vector>
 #include <cinttypes>
 
-class Denc
-{
-public:
-	Denc(int nbands = 3) : bands(nbands), prev(bands, 127) {};
-	// Reads a vector of values, stored as a vector of deltas
-	int delta(const std::vector<uint8_t>& input);
-	// Restores the values, returns 0 if other is identical to v;
-	int deltacheck(const std::vector<uint8_t>& other);
-	// Changes the internal vector of deltas into a small value positive
-	int recode();
-	// 
-	int bands;
-	std::vector<uint8_t> prev;
-	std::vector<uint8_t> v;
-};
-
+// block delta encode, sign and value, truncated binary
+// Returns the size of the encoded result
+size_t encode(std::vector<uint8_t> &image,
+	size_t xsize, size_t ysize,
+	size_t bsize = 4);
