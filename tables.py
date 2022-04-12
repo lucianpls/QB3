169134346080263951dd7266a70a2343266ab0c3
Lucian Plesea <LPlesea@esri.com>
2022-04-11 21:46:16 -0700

    Move signaling bits low

--- tables.py
+++ tables.py
@@ -28,40 +28,41 @@ def show_magsigns():
         for v in range(-2**rung, 2**rung):
             print(f"{v}, {magss(v):x}, {smags(magss(v))}")
 
+# Signaling is on the lowest 2 bits.
+# x 0 -> bottom quarter
+# 0 1 -> middle quarter
+# 1 1 -> top half
+
 # Returns a 16 bit value, top 4 bits is payload size
 # This system works up to rung 11
 def encode(v, rung):
+    if rung == 0:
+        return 0x1000 + v
     if v < (1 << (rung - 1)): # Short
-        return (rung << 12) + v + (1 << (rung - 1))
-    elif v < (1 << rung): # Middle, rotated right one bit
-        return ((rung + 1) << 12) + (v >> 1) + ((v & 1) << rung)
+        return (rung << 12) + v * 2
+    elif v < (1 << rung): # Middle
+        return ((rung + 1) << 12) + 4 * (v - (1 << (rung - 1))) + 1
     else: # Long, rotated right two bits
-        return ((rung + 2) << 12) + ((v - (1 << rung)) >> 2) + ((v & 3) << rung)
+        return ((rung + 2) << 12) + 4 * (v - (1 << rung)) + 3
 
 # Decode a var length unsigned int, returns value (lower rung - 1 bits) and source size bits 12+
 def decode(v, rung):
-    if v & (1 << (rung - 1)): # Short
-        return (rung << 12) | (v & ((1 << (rung - 1)) - 1))
-    if v & (1 << (rung - 2)): # Nominal
-        return ((rung + 1) << 12) | ((v << 1) & ((1 << rung) - 1) | ((v >> rung) & 1))
-    # long
-    return ((rung + 2) << 12) | ((v << 2) & ((1 << (rung + 1)) - 1) | (1 << rung) | ((v >> rung) & 3))
+    if rung == 0:
+        return 0x1000 + (v & 1)
+    if v & 1:
+        if v & 2:
+            v = v & ((1 << (rung + 2)) -1)
+            return ((rung + 2) << 12) + (v >> 2) + (1 << rung)
+        else:
+            v = v & ((1 << (rung + 1)) -1)
+            return ((rung + 1) << 12) + (v >> 2) + (1 << (rung - 1))
+    else: # Short
+        v = v & ((1 << rung) -1)
+        return (rung << 12) + (v >> 1)
 
-# tables for byte data
-def showencode8():
-    print("static const uint16_t crg1[] = {0x1000, 0x2003, 0x3001, 0x3005};")
-    for rung in range(2, 8):
-        s = f"static const uint16_t crg{rung}[] = {{"
-        for v in range(2**(rung+1)):
-            s += f"0x{encode(v, rung):4x}, "
-            if len(s) > 120:
-                print(s)
-                s = ""
-        print(s[:-2] + "};")
-
-# tables for rungs 8 to 10 (up to 11 bits, encoded fits in 12 bits)
-def showencode16():
-    for rung in range(8, 11):
+# tables for encoding, up to 11 bits
+def showencode():
+    for rung in range(11):
         s = f"static const uint16_t crg{rung}[] = {{"
         for v in range(2**(rung+1)):
             s += f"0x{encode(v, rung):4x}, "
@@ -71,8 +72,7 @@ def showencode16():
         print(s[:-2] + "};")
 
 def showdecode():
-    print("static const uint16_t drg1[] = {0x1000, 0x3002, 0x1000, 0x2001, 0x1000, 0x3003, 0x1000, 0x2001 };")
-    for rung in range(2, 11):
+    for rung in range(11):
         s = f"static const uint16_t drg{rung}[] = {{"
         for v in range(2**(rung + 2)):
             s += f"0x{decode(v, rung):04x}, "
@@ -80,6 +80,32 @@ def showdecode():
                 print(s)
                 s=""
         print(s[:-2] + "};")
+
+def trycodec():
+    'Check encoding and decoding tables for all rungs'
+    # Rung 0 is constant size
+    assert 0x1000 == encode(0, 0) and 0x1000 == decode(0, 0)
+    assert 0x1001 == encode(1, 0) and 0x1001 == decode(1, 0)
+
+    for rung in range(1, 11):
+        for v in range(2 ** (rung + 1)):
+            size = encode(v, rung) >> 12
+            assert size >= rung and size <= rung + 2, f"Size error at rung {rung}"
+            code = encode(v, rung) & 0xfff
+            assert (code >> size) == 0, "Encoding error at rung {rung}"
+
+            if v < (1 << (rung - 1)): # Short, 4 encodings
+                for x in range(4):
+                    assert v == 0xfff & decode(code + (x << rung), rung),\
+                        f"rung {rung}, {code + (x << rung):12b} decodes to {0xfff & decode(code + (x << rung), rung):12b}, expected {v:12b}"
+            elif v < (1 << rung): # Middle, 2 encodings
+                assert v == 0xfff & decode(code, rung),\
+                    f"rung {rung}, {code:12b} decodes to {0xfff & decode(code, rung):12b}, expected {v:12b}"
+                assert v == 0xfff & decode(code + (1 << (rung + 1)), rung),\
+                    f"rung {rung}, {code + (1 << (rung + 1)):12b} decodes to {0xfff & decode(code + (1 << (rung + 1)), rung):12b}, expected {v:12b}"
+            else: # Long, single encoding
+                assert v == 0xfff & decode(code, rung),\
+                    f"rung {rung}, {code:12b} decodes to {0xfff & decode(code, rung):12b}, expected {v:12b}"
 
 def cs(v, u):
     'convert delta to codeword for codeswitch'
@@ -193,11 +219,13 @@ def show_tripio():
     print_table(out, "static const uint8_t tripio[] = { ", "0x{:04x}, ")
 
 if __name__ == "__main__":
-    # showencode8()
-    # showencode16()
+    showencode()
+    showdecode()
+
+    trycodec()
+
     # showcodeswitch()
     # showdecodeswitch()
-    # showdecode()
     # showsame()
     # show_dopio()
-    show_tripio()
+    # show_tripio()
