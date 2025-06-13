481c55331eeade97eec02c1650b700da7adde8f9
Lucian Plesea <LPlesea@esri.com>
2025-06-13 12:06:37 -0700

    Add missing band mix for RGB, there are 10

--- cqb3.cpp
+++ cqb3.cpp
@@ -552,7 +552,7 @@ int encode_main(options& opts) {
         }
         string RGB_combo[] = { // Keep the alpha separate if it exists
             "1,1,1", "0,0,0", "0,0,2", "0,1,0", "0,1,1",
-            "0,1,2", "1,1,2", "2,1,2", "2,2,2"
+            "0,1,2", "0,2,2", "1,1,2", "2,1,2", "2,2,2"
         };
         opts.time = 0; // To start accumulating
         for (auto& combo : RGB_combo) {
